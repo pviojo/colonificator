e710d86aeadb422d281ee767a5104d20298f7fde
Pablo Viojo <pablo@tiopaul.io>
2020-02-13 14:53:33 -0300

    F

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {CopyToClipboard} from "react-copy-to-clipboard";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import {
   ThemeProvider,
   CSSReset,
@@ -13,46 +13,42 @@ import {
   Box
 } from "@chakra-ui/core";
 
-
-import styles from './index.module.css'
+import styles from "./index.module.css";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      output: '',
+      output: "",
       copied: false,
-      input: '',
+      input: ""
     };
   }
 
-  setInput = (value) => {
+  setInput = value => {
     this.setState({
-      input: value,
+      input: value
     });
-  }
+  };
 
   convertToColon = () => {
-    const lines = this.state.input.split('\n').filter((x) => !!x);
-    const output = lines.join(', ');
+    const lines = this.state.input.split("\n").filter(x => !!x);
+    const output = lines.join(", ");
     this.setState({
-      output,
+      output
     });
-  }
+  };
 
   convertToColonAndQuote = () => {
-    const lines = this.state.input.split('\n').filter((x) => !!x);
+    const lines = this.state.input.split("\n").filter(x => !!x);
     const output = `'${lines.join("', '")}'`;
     this.setState({
-      output,
-    });
-  }
-
-  render () {
-    const {
-      input,
       output
-    } = this.state;
+    });
+  };
+
+  render() {
+    const { input, output } = this.state;
     return (
       <ThemeProvider theme={theme}>
         <CSSReset />
@@ -62,22 +58,41 @@ class App extends React.Component {
               <FormControl>
                 <FormLabel>Input</FormLabel>
                 <FormErrorMessage>Error message</FormErrorMessage>
-                <Textarea size="lg" resize="horizontal" style={{height:'15rem'}} value={input} onChange={(e) => this.setInput(e.target.value) }/>
+                <Textarea
+                  size="lg"
+                  resize="horizontal"
+                  style={{ height: "15rem" }}
+                  value={input}
+                  onChange={e => this.setInput(e.target.value)}
+                />
                 <FormHelperText>One item per line</FormHelperText>
               </FormControl>
             </Box>
             <Box pb={8}>
-              <Button variant="solid" onClick={this.convertToColon} mr={4}>Comma separated</Button>
-              <Button variant="solid" onClick={this.convertToColonAndQuote}>Comma separated + single quotes</Button>
+              <Button variant="solid" onClick={this.convertToColon} mr={4}>
+                Comma separated
+              </Button>
+              <Button variant="solid" onClick={this.convertToColonAndQuote}>
+                Comma separated + single quotes
+              </Button>
             </Box>
             <Box>
               <FormControl>
                 <FormLabel>Output</FormLabel>
-                <CopyToClipboard text={output}
-                  onCopy={() => this.setState({copied: true})}>
-                  <span className={styles.copy}>{this.state.copied ? 'Copied!' : 'Copy'}</span>
+                <CopyToClipboard
+                  text={output}
+                  onCopy={() => this.setState({ copied: true })}
+                >
+                  <span className={styles.copy}>
+                    {this.state.copied ? "Copied!" : "Copy"}
+                  </span>
                 </CopyToClipboard>
-                <Textarea size="lg" resize="horizontal" style={{height:'15rem'}} value={output}/>
+                <Textarea
+                  size="lg"
+                  resize="horizontal"
+                  style={{ height: "15rem" }}
+                  value={output}
+                />
               </FormControl>
             </Box>
           </Box>
@@ -85,6 +100,6 @@ class App extends React.Component {
       </ThemeProvider>
     );
   }
-};
+}
 
 export default App;
